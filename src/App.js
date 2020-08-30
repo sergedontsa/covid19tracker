5cb3a0a227a18252f55dcd225ab5843634cb28f1
sergedontsa <63218303+sergedontsa@users.noreply.github.com>
2020-08-30 17:20:15 -0400

    commit m

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./Component/Header/Header";
 import HomePage from "./Pages/HomePage/HomePage";
-import AboutPage from "./Pages/AboutPage/AboutPage";
-import NewsPage from "./Pages/NewsPage/NewsPage";
+
 import {Route, Switch} from 'react-router-dom'
 
 function App() {
@@ -12,9 +11,7 @@ function App() {
         <Header/>
         <Switch>
             <Route path="/" exact={true} component={HomePage}/>
-            <Route path="/covid19tracker" component={HomePage}/>
-            <Route path="/news" exact={true} component={NewsPage}/>
-            <Route path="/about" exact={true} component={AboutPage}/>
+
         </Switch>
     </div>
   );
